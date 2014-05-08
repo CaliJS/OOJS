91e25d92cde8841dc22b6197697c140d1d0113b2
Felipe <fmendez1212@gmail.com>
2014-05-07 23:13:12 -0500

    contructor function prototipe inheritance

--- herenciaPrototipos.js
+++ herenciaPrototipos.js
@@ -1,28 +1,53 @@
-var person = {
-	firstName : 'John',
-	lastName : 'Doe'
-};
-
-Object.defineProperty(person, 'fullName', {
-	get : function() {
-		'use strict';
-
-		return this.firstName + ' ' + this.lastName;
-	}
-});
-
-var employee = Object.create(person, {
-	sayHi : {
-		value: function() {
-			'use strict';
-			return this.__proto__.sayHi.call(this) + '. My name is ' + this.fullName;
- 		},
-		writable : false,
-		enumerable : true
-	}
-});
-
-person.sayHi = function() {
+var Person = function(firstName, lastName) {
 	'use strict';
-	return 'Hi there';
+
+	this.firstName = firstName;
+	this.lastName = lastName;
 };
+
+Object.defineProperties(Person.prototype, {
+	sayHi :{
+		value : function() {
+			'use strict';
+
+			return 'Hi there.';
+		}
+	},
+	fullName : {
+		get : function() {
+			'use strict';
+
+			return this.firstName + ' ' + this.lastName;
+		},
+		enumerable : true
+	} 
+});
+
+var Employee = function(firstName, lastName, position) {
+	'use strict';
+
+	Person.call(this, firstName, lastName);
+	this.position = position;
+};
+
+Employee.prototype = Object.create(Person.prototype, {
+	sayHi :{
+		value : function() {
+			'use strict';
+
+			return Person.prototype.sayHi.call(this) + ' My name is ' + this.fullName;
+		}
+	},
+	fullName : {
+		get : (function() {
+			'use strict';
+			var desc = Object.getOwnPropertyDescriptor(Person.prototype, 'fullName').get;
+			return function () {
+				return desc.call(this) + ', ' + this.position;
+			};
+		}()),
+		enumerable : true
+	} 
+});
+
+var johnDoe = new Employee('John', 'Doe', 'Manager');
